668a8420be420301bdde821ec9cb92b3165ba863
mikaelmcast <63060375+mikaelmcast@users.noreply.github.com>
2020-12-28 14:01:00 +0100

    Scrolling Background and Camera border
    Scrolling Background and Camera border using ViewPortToWorldPoint(), Quad, Offset and Materials

---
+++ HA/2DCarGame/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float xMin, xMax, yMin, yMax;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBounderies();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SetUpMoveBounderies()
+    {
+        Camera worldCamera = Camera.main;
+
+        xMin = worldCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+
+        xMax = worldCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+
+        yMin = worldCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+
+        yMax = worldCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+    }
+}
