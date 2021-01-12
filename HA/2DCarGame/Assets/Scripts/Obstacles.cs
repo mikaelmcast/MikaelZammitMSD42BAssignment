a623f511a32c9b2a5f12a1027a25e2b5796e5983
mikaelmcast <63060375+mikaelmcast@users.noreply.github.com>
2021-01-12 13:05:47 +0100

    sound effects implementation
    sound effects

--- HA/2DCarGame/Assets/Scripts/Obstacles.cs
+++ HA/2DCarGame/Assets/Scripts/Obstacles.cs
@@ -13,7 +13,21 @@ public class Obstacles : MonoBehaviour
     [SerializeField] GameObject obstacleBulletPrefab;
 
     [SerializeField] float obstacleBulletSpeed = 0.3f;
+
+    [SerializeField] float health = 500f;
+
+    [SerializeField] AudioClip ObstacleDeathSound;
+    [SerializeField] [Range(0, 1)] float ObstacleDeathSoundVolume = 0.75f;
+
     // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        DamageDealer dmgDealer = otherObject.gameObject.GetComponent<DamageDealer>();
+
+        ProcessHit(dmgDealer);
+    }
+
     void Start()
     {
         shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
@@ -43,5 +57,22 @@ public class Obstacles : MonoBehaviour
         GameObject obstacleBullet = Instantiate(obstacleBulletPrefab, transform.position, Quaternion.identity) as GameObject;
 
         obstacleBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -obstacleBulletSpeed);
+    }
+
+    private void ProcessHit(DamageDealer dmgDealer)
+    {
+        health -= dmgDealer.GetDamage();
+
+        AudioSource.PlayClipAtPoint(ObstacleDeathSound, Camera.main.transform.position, ObstacleDeathSoundVolume);
+
+        if (health <= 0)
+        {
+            Destroyed();
+        }
+    }
+
+    private void Destroyed()
+    {
+        Destroy(gameObject);
     }
 }
